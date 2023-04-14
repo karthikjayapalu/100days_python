1bbe68e58089a5f1adf410df8837c283c5e5b30a
KRM5KOR <karthigeyan.ramachandranjayapalu@in.bosch.com>
2023-04-14 19:22:07 +0530

    day 24

--- 20_th_Day_Snake_Game/main.py
+++ 20_th_Day_Snake_Game/main.py
@@ -37,8 +37,9 @@ while game_is_on:
 
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
+
 
     # Detect collision with tail
         # without slicing concept
@@ -53,8 +54,7 @@ while game_is_on:
     # with slicing concept
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
-
-
+            score.reset()
+            snake.reset()
+            # score.game_over()
 screen.exitonclick()
