8882df60983efcf6aa4339f20e3a0809277bfc33
karthikjayapalu <karthikjayapalu@gmail.com>
2023-05-21 19:33:12 +0530

    25th day +26th day

--- 25_th_Day_Pandas/us-states-game-start/main.py
+++ 25_th_Day_Pandas/us-states-game-start/main.py
@@ -10,20 +10,39 @@ turtle.shape(image)
 
 data= pd.read_csv('50_states.csv')
 
-answer_state=screen.textinput(title="Guess the state",prompt="What's another state's name?")
-print(answer_state)
 
-# If answer_state is one of the states in all the states of the 50_states.csv
-if data['state'] == answer_state:
-    #If they got it right:
+all_states=data.state.to_list()
+guessed_states=[]
+# print(answer_state)
 
-    t=turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data=data[data['state'] == answer_state]
-    t.goto(int(state_data.x),int(state_data.y))
-    # Create a turtle to write  the name of the state at the state's x and y coordinate in the image
-    t.write(answer_state)
+while len(guessed_states)<50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct ",
+                                    prompt="What's another state's name?").title()
+    # If answer_state is one of the states in all the states of the 50_states.csv
+    if answer_state =="Exit":
+
+        # states to learn.csv
+        missing_states=[]
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(missing_states)
+        new_data=pd.DataFrame(missing_states)
+        new_data.to_csv('states_to_learn.csv')
+        break
+    if answer_state in all_states:
+        #If they got it right:
+        guessed_states.append(answer_state)
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data=data[data['state'] == answer_state]
+        t.goto(int(state_data.x),int(state_data.y))
+        # Create a turtle to write  the name of the state at the state's x and y coordinate in the image
+        t.write(answer_state)
+
+
+
 
 # TODO 1: Convert the guess to Title case
 # TODO 2: Check if the guess is among the 50 states
